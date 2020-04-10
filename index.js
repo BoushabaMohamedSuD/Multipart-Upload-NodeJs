02c0e73401220101e271da9cebeb6ed6777fc26e
Boushaba Mohamed <med1998yz@gmail.com>
2020-04-10 16:37:35 +0200

    aws-sdk

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+AWS = require("aws-sdk");
+
 console.log("hello mohamed");
+
+
+
+AWS.config.getCredentials(function (err) {
+    if (err) console.log(err.stack);
+    // credentials not loaded
+    else {
+        console.log("Access key:", AWS.config.credentials.accessKeyId);
+        console.log("Secret access key:", AWS.config.credentials.secretAccessKey);
+    }
+});
+
+
+console.log("Region: ", AWS.config.region);
