ee4fa9ca96b601a668aa61c12c54b29000c4ab25
Boushaba Mohamed <med1998yz@gmail.com>
2020-04-10 16:48:49 +0200

    s3 listParts

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
-AWS = require("aws-sdk");
+var AWS = require("aws-sdk");
+
+var fs = require('fs');
+
+
+
+var s3 = new AWS.S3();
+
+// File
+//var fileName = '5.pdf';
+//var filePath = './' + fileName;
+//var fileKey = fileName;
+//var buffer = fs.readFileSync('./' + filePath);
+// S3 Upload options
+//var bucket = 'loctest';
+
+
+var params = {
+    Bucket: "boushaba-archives",
+    Key: "souvenir.rar",
+    UploadId: "QJ5_pr8yVmiE7OHz6rGi.VDMaBQEpXW0C6z1iic3XeHfu8.akrCvIbpdvAOIEKptXbcS1NEXj5pILNLIUt2slZOU4Z0vWytJBko33NGilQDcI4XAUMQZV3nGS5J3_Oi_"
+};
 
 console.log("hello mohamed");
 
@@ -15,3 +36,12 @@ AWS.config.getCredentials(function (err) {
 
 
 console.log("Region: ", AWS.config.region);
+
+
+
+s3.listParts(params)
+
+s3.listParts(params, function (err, data) {
+    if (err) console.log(err, err.stack);
+    else console.log(data);
+});
